a9fe389ccd5a3da7a4486fc46a694f594ec20adc
thevalidator <the.validator@yandex.ru>
2023-03-10 15:56:07 +0300

    Update BeautifulString.java

--- src/main/java/ru/thevalidator/yandex/algotraining/v3/a/BeautifulString.java
+++ src/main/java/ru/thevalidator/yandex/algotraining/v3/a/BeautifulString.java
@@ -6,54 +6,64 @@ package ru.thevalidator.yandex.algotraining.v3.a;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * @author thevalidator <the.validator@yandex.ru>
  */
 public class BeautifulString {
 
-    public static void main(String[] args) throws IOException {
-        int max = 0;
-        int k = 2;
-        String s = "helto";//"ababababababababababababababababababababababababababababababababababababababababababababababababababbcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    public static void main(String[] args) {
+        try ( BufferedReader br = Files.newBufferedReader(Paths.get("task.txt"));) {
+            //try ( BufferedReader br = new BufferedReader(new InputStreamReader(System.in));) {
 
-        //"helto";//"aaaiaaiiaa";//"acvvvvkjvvvvuuiiiiiii";//abcaaaaaaz";//"heltoe";
-        //System.out.println(k + " " + s);
-        //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        //int k = Integer.parseInt(br.readLine());
-        //String s = br.readLine();
-//        while ((line = br.readLine()) != null) {
-//            if (line.charAt(0) == '0') {
-//                int number = Integer.parseInt(line.substring(2));
-//                heap.insert(number);
-//            } else {
-//                System.out.println(heap.extract());
-//            }
-//        }
-        System.out.println(s);
-        int replacements = k;
-        int right = 0;
-        System.out.println("rep:" + k);
-        
-        for (int left = 0; left < s.length(); left++) {
+            int k = 1;//Integer.parseInt(br.readLine());
+            String s = "abazaaa";//br.readLine();
 
-            System.out.println("left:" + left);
-            if (left > 0 && (int) s.charAt(left) != 't') {
-                replacements++;
+            System.out.println(calc(k, s));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static int calc(int k, String s) {
+
+        if ((s.length() - k) < 2) {
+            return s.length();
+        }
+
+        int repeat = 0;
+        for (char c = 'a'; c <= 'z'; c++) {
+            System.out.println(c);
+            
+            int first = 0;
+            int second = 0;
+            int replacements = k;
+
+            for (int i = 0; i < s.length(); i++) {
+                System.out.println("\ti=" + i);
+                while (replacements >= 0) {
+                    if (second >= s.length()) {
+                        second++;
+                        break;
+                    }
+                    if (c != s.charAt(second)) {
+                        replacements--;
+                    }
+                    second++;
+                }
+                
+                if (s.charAt(first++) != c) {
+                    replacements++;
+                }
+                repeat = second - first > repeat ? second - first : repeat;
             }
-            //System.out.println("rep:" + replacements);
-            
-            while (replacements >= 0 && right < s.length()) {
-                if (s.charAt(right) != 't') {
-                    replacements--;
-                } 
-                right++;
-            }
-            
-            
-            System.out.println("max:" + (right - left) + " r:" + (right) + " l:" + left);
 
         }
+
+        return repeat;
 
     }
 }
